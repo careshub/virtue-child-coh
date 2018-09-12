7aa3474e691eb08fd88c9ecba2a6253acc328d2e
David Cavins <david.cavins@gmail.com>
2018-09-12 10:36:57 -0500

    Add GTM container scripts.

--- functions.php
+++ functions.php
@@ -37,3 +37,40 @@ function coh_register_sidebars() {
 	) );
 }
 add_action( 'widgets_init', 'coh_register_sidebars', 99 );
+
+/**
+ * Add the Google "noscript" tag immediately after the opening of the body element.
+ *
+ * @since 1.0.1
+ *
+ */
+function cares_hestia_child_add_google_tag_manager_noscript_tag() {
+	// CARES Network Sites container ID: GTM-PQGZB4S
+	?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PQGZB4S"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<?php
+}
+add_action( 'virtue_after_body', 'cares_hestia_child_add_google_tag_manager_noscript_tag' );
+
+/**
+ * Add the Google Tag Manager script call.
+ *
+ * @since 1.0.1
+ *
+ */
+function cares_hestia_child_add_google_tag_manager_script() {
+	// CARES Network Sites container ID: GTM-PQGZB4S
+	?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PQGZB4S');</script>
+	<!-- End Google Tag Manager -->
+	<?php
+}
+add_action( 'wp_head', 'cares_hestia_child_add_google_tag_manager_script' );
